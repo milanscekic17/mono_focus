1c2dce0847aa5468ddd14edd0865704b4304ba8a
Milan Scekic <milanscekic@hotmail.com>
2025-07-01 12:28:56 +0800

    Fixed build file

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Building executable...
-pyinstaller --clean --onefile --noconsole --icon=icon.ico  mono_focus.py
+pyinstaller --clean --onefile --icon=icon.ico  mono_focus.py
 echo Done. Check the dist\ folder.
 pause
